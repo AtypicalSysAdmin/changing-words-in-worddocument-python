29f0f4b88a2f5a56a6dbcd5dd387dc353fe191f0
Mohammad Rahmani <83430369+MohammadRK2000@users.noreply.github.com>
2021-11-27 16:00:10 -0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,6 @@
 
 
-#!pip install python-docx
+#pip install python-docx
 #start from here if python-docx is installed
 from docx import Document
 #indentation lib
@@ -10,18 +10,21 @@ from docx.shared import Pt
 #open the document
 document=Document('C:\Program Files\Python Programs\cover-letter\cover-letter-python\PythonApplication1\PythonApplication1\\test.docx')
 newword=input('Enter Company Name:')
+#getting the style object of the doc
 style = document.styles['Normal']
+#modifying the style
 font = style.font
 font.name = 'Calibri'
 font.size = Pt(12)
 for paragraph in document.paragraphs:
     if "your Company" in paragraph.text:
         print (paragraph.text)
-        
+        #replacing your word or sentence with another string
         paragraph.text=paragraph.text.replace("your Company", newword)
         #paragraph.paragraph_format.line_spacing = Inches(0.5)
         print('=======================')
         print(paragraph.text)
+        #assigning the created style to the edited paragraph
         paragraph.style = document.styles['Normal']
 #save changed document
 
